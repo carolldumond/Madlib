1b7abb5cdf0e0ac540b57e636f0bdfb00bee4e4e
Gae Drumond <carolldrumond16@gmail.com>
2021-04-14 17:39:38 -0300

    MadLib Project

---
+++ main.py
@@ -0,0 +1,27 @@
+# MadLib project
+
+
+adj = input("Adjective: ")
+hero = input("Hero name: ")
+verb1 = input("Verb: ")
+color = input("Color: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+verb4 = input("Verb: ")
+famous_person = input("Famous person name: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+
+
+holding_out_for_a_hero = f" Where have all the {adj} men gone and where are all the Gods? \
+\n Where's the street wise {hero} to {verb1} the rising odds? \
+\n Isn't there a {color} knight upon a fiery steed? \
+\n Late at night I {verb2} and I {verb3} and I {verb4} of what I need \
+\n I need {famous_person} \
+\n I'm holding out for {famous_person} 'til the end of the night \
+\n He's gotta be {adj2} and he's gotta be {adj3} \
+\n And he's gotta be {adj4} from the fight"
+
+
+print(holding_out_for_a_hero)
